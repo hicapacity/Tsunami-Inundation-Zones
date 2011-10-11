1c055ddac2a0ed1997e7812773e4a5b68f0bafae
Jason Axelson <jason.axelson@gmail.com>
2011-10-10 21:30:08 -1000

    Initial version of Android app

---
+++ Android/src/org/hicapacity/tsunami/TsunamiZonesActivity.java
@@ -0,0 +1,13 @@
+package org.hicapacity.tsunami;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TsunamiZonesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
